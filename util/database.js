75afe2c9a44de445c470d3b7f16b3c6dde0508d7
Eric Ross <epr@rossmbox.com>
2024-09-25 20:09:14 -0700

    update driver

--- util/database.js
+++ util/database.js
@@ -1,9 +1,12 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+const DBUSER = process.env.DBUSER;
+const DBPASSWORD = process.env.DBPASSWORD;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    `mongodb+srv://${DBUSER}:${DBPASSWORD}@sandbox.wgeng.mongodb.net/?retryWrites=true&w=majority&appName=sandbox`
   )
     .then(client => {
       console.log('Connected!');
